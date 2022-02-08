9498c1ba3c0a3a11a29f42edb5911eeb7bbd6dcf
Umar Syyid <usyyid@rev-lock.com>
2022-02-08 09:18:51 -0500

    Fix problem with states in AWS

--- src/tasks/wait-until-not-pending.js
+++ src/tasks/wait-until-not-pending.js
@@ -1,24 +1,37 @@
 const retry = require('oh-no-i-insist');
+const isDebugEnabled = process.env.DEBUG && process.env.DEBUG.includes("claudia-retry");
+
 module.exports = function waitUntilNotPending(lambda, functionName, timeout, retries) {
-	'use strict';
-	return retry(
-		() => {
-			return lambda.getFunctionConfiguration({FunctionName: functionName}).promise()
-				.then(result => {
-					if (result.state === 'Failed') {
-						throw `Lambda resource update failed`;
-					}
-					if (result.state === 'Pending') {
-						throw 'Pending';
-					}
-				});
-		},
-		timeout,
-		retries,
-		failure => failure === 'Pending',
-		() => console.log('Lambda function is in Pending state, waiting...'),
-		Promise
-	);
+    'use strict';
+    return retry(
+        () => {
+            return lambda.getFunctionConfiguration({FunctionName: functionName}).promise()
+                .then(result => {
+                    if (isDebugEnabled)
+                        console.error("Retrying.. !!", result);
+
+                    if (result.LastUpdateStatus === 'Failed') {
+                        throw `Lambda resource update failed`;
+                    }
+                    if (result.LastUpdateStatus === 'InProgress') {
+                        throw 'Pending';
+                    } else {
+                        if (isDebugEnabled)
+                            console.error("Unknown state!!!", result.LastUpdateStatus)
+                    }
+                });
+        },
+        timeout,
+        retries,
+        (status) => {
+            if (isDebugEnabled)
+                console.error("Testing retry with status: ", status);
+
+            return (status === 'Pending')
+        },
+        () => console.log('Lambda function is in Pending state, waiting...'),
+        Promise
+    );
 };
 
 
